fa00fb2a36a1eb07764be57fd429853c875ed752
Brian Hague <alphaseinor@gmail.com>
2019-09-13 00:08:38 -0500

    added axios call to my github account, did a try catch finally

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,17 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get(`https://api.github.com/users/alphaseinor`)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  .finally(x => {
+    console.log(`now deal wit' it!`)
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
